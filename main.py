e3d8890d84fef6287166d0d37f40edd30f471a97
Jason Liu <jasondasmexyboi@gmail.com>
2021-06-19 00:54:01 -0400

    Continuous function checking for jobs, used as a test

---
+++ main.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+def does_exist(filter_skills, skills):
+    if filter_skills[0] == '':
+        return True
+    for skill in filter_skills:
+        if skill in skills:
+            return False
+    return True
+
+print('Enter a filter out skill (Separate all skills with a comma)')
+filter_out_skill = input('>').lower().strip().split(',')
+if len(filter_out_skill) >= 1 and filter_out_skill[0] != '':
+    print(f"Filtering out {','.join(filter_out_skill)}...")
+else:
+    print("Finding jobs...")
+
+def find_jobs():
+    r = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    bs = BeautifulSoup(r, 'lxml')
+    jobs = bs.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    results = 0
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        valid = True
+        if 'few' in published_date:
+            company = job.find('h3', class_ = 'joblist-comp-name').text.strip()
+            skills = job.find('span', class_ = 'srp-skills').text.strip()
+            more_info = job.header.h2.a['href']
+            if does_exist(filter_out_skill, skills):
+                print(f"Company Name: {company}")
+                print(f"Required Skills: {skills.replace(' ', '')}")
+                print(f"More info: {more_info}")
+                results += 1
+
+                print('')
+
+    print(f"Found {results} results")
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = .1
+        time.sleep(60*time_wait)
+        print(f"Refreshing search...")
