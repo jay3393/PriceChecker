22c177da17a68d9dfb473eaca08ba9133e5b34a6
Jason Liu <jasondasmexyboi@gmail.com>
2021-06-21 00:25:24 -0400

    added a products text file to contain all the products wanted to check

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-url1 = 'https://www.bestbuy.com/site/razer-kraken-ultimate-wired-thx-spatial-audio-gaming-headset-for-pc-with-rgb-lighting-classic-black/6391902.p?skuId=6391902'
-
-url2= 'https://www.walmart.com/ip/Razer-Kraken-X-Multi-Platform-Wired-Gaming-Headset-Black/323390578'
-
-url3 = 'https://www.microcenter.com/product/615492/razer-kraken-x-wired-gaming-headset'
-
-url4 = 'https://www.microcenter.com/product/627723/turtle-beach-stealth-700-gen-2-premium-wireless-gaming-headset'
-
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
 }
@@ -63,7 +55,7 @@ def microcenter_check_price(URL):
         product = details[1].text
         price = soup.find('span', id='pricing').text
     except:
-        print('Failed to retrieve data')
+        print('Failed to retrieve data\n')
         return
 
     print_product_info(product, price, URL)
@@ -80,31 +72,24 @@ def case_switch(arg, URL):
 
 def find_site(URL=None):
     '''Use regex to determine the function to call based on the URL'''
-    # text =  '''
-    # https://www.bestbuy.com/site/razer-kraken-ultimate-wired-thx-spatial-audio-gaming-headset-for-pc-with-rgb-lighting-classic-black/6391902.p?skuId=6391902
-    # https://www.walmart.com/ip/Razer-Kraken-X-Multi-Platform-Wired-Gaming-Headset-Black/323390578
-    # https://www.microcenter.com/product/615492/razer-kraken-x-wired-gaming-headset
-    # https://www.microcenter.com/product/627723/turtle-beach-stealth-700-gen-2-premium-wireless-gaming-headset
-    # https://www2.hm.com/en_us/productpage.0685816099.html
-    # https://www.target.com/c/patio-furniture-garden/all-deals/-/N-5xtorZakkos?type=products
-    # https://www.aliexpress.com/item/1005002642578572.html?spm=a2g0o.productlist.0.0.35b47183j62gRT&aem_p4p_detail=202106201502137698753256523000029143816
-    # https://www.alibaba.com/product-detail/1-Pc-Body-Fat-Tester-Analyzer_1600128381046.html?spm=a27aq.22883793.4119238120.13.2709bda5UKlcmS&ecology_token=default
-    # '''
     pattern = re.compile(r'\w+\.(com|net)')
     filter = pattern.search(URL)
-    # for match in filter:
-    #     print(match)
-    #print(filter.group())
     case_switch(str(filter.group()), URL)
 
 
 if __name__ == '__main__':
+    with open('products.txt', 'r') as f:
+        lines = f.read().splitlines()
     while True:
         print('Updating prices...')
-        find_site(url1)
-        find_site(url2)
-        find_site(url3)
-        find_site(url4)
-        print('Found results')
+        start_time = time.time()
+
+        for line in lines:
+            find_site(line)
+
+        time_taken = time.time() - start_time
+        print(f'Found results ({time_taken} secs)')
         delay = .1
         time.sleep(60*delay)
+
+    f.close()
