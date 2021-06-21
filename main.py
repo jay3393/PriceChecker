bd1303bcc0dbd6bd405749743a047f9a13ebcea9
Jason Liu <jasondasmexyboi@gmail.com>
2021-06-21 03:34:15 -0400

    added indicator of success

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class PriceChecker:
     }
 
     console_log = ""
+    tasks = 0
+    completed_tasks = 0
 
     def print_product_info(self, name, price, url):
         # print('==================================================')
@@ -32,6 +34,8 @@ class PriceChecker:
         try:
             product = soup.find('h1', class_='heading-5 v-fw-regular').text
             price = soup.find('div', class_='priceView-hero-price priceView-customer-price').find('span').text
+            print(f'Success! {URL}')
+            self.completed_tasks += 1
         except:
             print('Failed to retrieve data')
             return
@@ -47,6 +51,8 @@ class PriceChecker:
         try:
             product = soup.find('h1', class_='prod-ProductTitle prod-productTitle-buyBox font-bold').text
             price = soup.find('span', class_='price display-inline-block arrange-fit price price--stylized').find('span', class_ = 'visuallyhidden').text
+            print(f'Success! {URL}')
+            self.completed_tasks += 1
         except:
             print(f'Failed to retrieve data for {URL}')
             return
@@ -63,6 +69,8 @@ class PriceChecker:
             details = soup.find('div', id='details').findAll('span')
             product = details[1].text
             price = soup.find('span', id='pricing').text
+            print(f'Success! {URL}')
+            self.completed_tasks += 1
         except:
             print('Failed to retrieve data\n')
             return
@@ -82,7 +90,6 @@ class PriceChecker:
     def find_site(self, URL):
         '''Use regex to determine the function to call based on the URL'''
         pattern = re.compile(r'\w+\.(com|net)')
-        print(URL)
         filter = pattern.search(URL)
         self.case_switch(str(filter.group()), URL)
 
@@ -91,8 +98,10 @@ if __name__ == '__main__':
     checker = PriceChecker()
     with open('products.txt', 'r') as f:
         lines = f.read().splitlines()
+    f.close()
+    checker.tasks = len(lines)
     while True:
-        #console_log = ""
+        checker.completed_tasks = 0
         print('Updating prices...')
         start_time = time.time()
 
@@ -103,8 +112,6 @@ if __name__ == '__main__':
         checker.console_log = ""
 
         time_taken = time.time() - start_time
-        print(f'Found results ({time_taken} secs)')
-        delay = .1
+        print(f'Found results ({checker.completed_tasks}/{checker.tasks}) in {time_taken} secs')
+        delay = .2
         time.sleep(60*delay)
-
-    f.close()
